a35961d48c9c8c3c5a7c0aec1f10897a49595577
Daniel Larimer <dlarimer@gmail.com>
2013-12-09 22:51:16 -0500

    factor pts_address into separate files

--- include/bts/address.hpp
+++ include/bts/address.hpp
@@ -41,34 +41,15 @@ namespace bts
    inline bool operator != ( const address& a, const address& b ) { return a.addr != b.addr; }
    inline bool operator <  ( const address& a, const address& b ) { return a.addr <  b.addr; }
 
-   struct pts_address
-   {
-       pts_address(); ///< constructs empty / null address
-       pts_address( const std::string& base58str );   ///< converts to binary, validates checksum
-       pts_address( const fc::ecc::public_key& pub ); ///< converts to binary
-
-       bool is_valid()const;
-
-       operator std::string()const; ///< converts to base58 + checksum
-
-       fc::array<char,25> addr;      ///< binary representation of address
-   };
-
-   inline bool operator == ( const pts_address& a, const pts_address& b ) { return a.addr == b.addr; }
-   inline bool operator != ( const pts_address& a, const pts_address& b ) { return a.addr != b.addr; }
-   inline bool operator <  ( const pts_address& a, const pts_address& b ) { return a.addr <  b.addr; }
 
 } // namespace bts
 
 FC_REFLECT( bts::address, (addr) )
-FC_REFLECT( bts::pts_address, (addr) )
 
 namespace fc 
 { 
    void to_variant( const bts::address& var,  fc::variant& vo );
    void from_variant( const fc::variant& var,  bts::address& vo );
-   void to_variant( const bts::pts_address& var,  fc::variant& vo );
-   void from_variant( const fc::variant& var,  bts::pts_address& vo );
 }
 
 namespace std
@@ -78,17 +59,6 @@ namespace std
    {
        public:
          size_t operator()(const bts::address &a) const 
-         {
-            size_t s;
-            memcpy( (char*)&s, &a.addr.data[sizeof(a)-sizeof(s)], sizeof(s) );
-            return s;
-         }
-   };
-   template<>
-   struct hash<bts::pts_address> 
-   {
-       public:
-         size_t operator()(const bts::pts_address &a) const 
          {
             size_t s;
             memcpy( (char*)&s, &a.addr.data[sizeof(a)-sizeof(s)], sizeof(s) );
--- src/address.cpp
+++ src/address.cpp
@@ -54,54 +54,6 @@ namespace bts
         return fc::to_base58( addr.data, sizeof(addr) );
    }
 
-
-   pts_address::pts_address()
-   {
-      memset( addr.data, 0, sizeof(addr.data) );
-   }
-
-   pts_address::pts_address( const std::string& base58str )
-   {
-      std::vector<char> v = fc::from_base58( fc::string(base58str) );
-      if( v.size() )
-         memcpy( addr.data, v.data(), std::min<size_t>( v.size(), sizeof(addr) ) );
-
-      if( !is_valid() )
-      {
-         FC_THROW_EXCEPTION( exception, "invalid pts_address ${a}", ("a", base58str) );  
-      }
-   }
-
-   pts_address::pts_address( const fc::ecc::public_key& pub )
-   {
-       auto dat      = pub.serialize_ecc_point();
-       auto sha2     = fc::sha256::hash(dat.data, sizeof(dat) );
-       auto rep      = fc::ripemd160::hash((char*)&sha2,sizeof(sha2));
-       addr.data[0]  = 56;
-       memcpy( addr.data+1, (char*)&rep, sizeof(rep) );
-       auto check    = fc::sha256::hash( addr.data, sizeof(rep)+1 );
-       check = fc::sha256::hash(check); // double
-       memcpy( addr.data+1+sizeof(rep), (char*)&check, 4 );
-   }
-
-   /**
-    *  Checks the address to verify it has a 
-    *  valid checksum and prefix.
-    */
-   bool pts_address::is_valid()const
-   {
-       if( addr.data[0]  != 56 ) return false;
-       auto check    = fc::sha256::hash( addr.data, sizeof(fc::ripemd160)+1 );
-       check = fc::sha256::hash(check); // double
-       return memcmp( addr.data+1+sizeof(fc::ripemd160), (char*)&check, 4 ) == 0;
-   }
-
-   pts_address::operator std::string()const
-   {
-        return fc::to_base58( addr.data, sizeof(addr) );
-   }
-
-
 } // namespace bts
 
 
