8862b9d97da965e016fc80d87c70f7a3e077b7dd
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-01-03 13:24:13 -0500

    adding missing mail files

---
+++ src/mail/mail_connection.cpp
@@ -0,0 +1,272 @@
+#include <mail/mail_connection.hpp>
+#include <mail/message.hpp>
+#include <bts/config.hpp>
+
+#include <fc/network/tcp_socket.hpp>
+#include <fc/network/resolve.hpp>
+#include <fc/reflect/variant.hpp>
+#include <fc/thread/thread.hpp>
+#include <fc/io/raw.hpp>
+#include <fc/log/logger.hpp>
+#include <fc/string.hpp>
+#include <fc/thread/mutex.hpp>
+#include <fc/thread/scoped_lock.hpp>
+
+#include <unordered_map>
+#include <bts/db/level_map.hpp>
+#include <bts/bitchat/bitchat_private_message.hpp>
+
+namespace mail {
+
+  namespace detail
+  {
+     class connection_impl
+     {
+        public:
+          connection_impl(connection& s)
+          :self(s),con_del(nullptr){}
+          connection&          self;
+          stcp_socket_ptr      sock;
+          fc::ip::endpoint     remote_ep;
+          connection_delegate* con_del;
+
+          fc::time_point _sync_time;
+          bts::db::level_map<fc::time_point,bts::bitchat::encrypted_message>*   _db;
+
+          /** used to ensure that messages are written completely */
+          fc::mutex              write_lock;
+
+
+          fc::future<void>       read_loop_complete;
+          fc::future<void>       exec_sync_loop_complete;
+
+          void read_loop()
+          {
+            const int BUFFER_SIZE = 16;
+            const int LEFTOVER = BUFFER_SIZE - sizeof(message_header);
+            try {
+               message m;
+               while( true )
+               {
+                  char tmp[BUFFER_SIZE];
+                  sock->read( tmp, BUFFER_SIZE );
+                  memcpy( (char*)&m, tmp, sizeof(message_header) );
+                  m.data.resize( m.size + 16 ); //give extra 16 bytes to allow for padding added in send call
+                  memcpy( (char*)m.data.data(), tmp + sizeof(message_header), LEFTOVER );
+                  sock->read( m.data.data() + LEFTOVER, 16*((m.size -LEFTOVER + 15)/16) );
+
+                  try { // message handling errors are warnings... 
+                    con_del->on_connection_message( self, m );
+                  } 
+                  catch ( fc::canceled_exception& e ) { throw; }
+                  catch ( fc::eof_exception& e ) { throw; }
+                  catch ( fc::exception& e ) 
+                  { 
+                     wlog( "disconnected ${er}", ("er", e.to_detail_string() ) );
+                     // TODO: log and potentiall disconnect... for now just warn.
+                  }
+               }
+            } 
+            catch ( const fc::canceled_exception& e )
+            {
+              if( con_del )
+              {
+                con_del->on_connection_disconnected( self );
+              }
+              else
+              {
+          //      wlog( "disconnected ${e}", ("e", e.to_detail_string() ) );
+              }
+            }
+            catch ( const fc::eof_exception& e )
+            {
+              if( con_del )
+              {
+                con_del->on_connection_disconnected( self );
+              }
+              else
+              {
+                wlog( "disconnected ${e}", ("e", e.to_detail_string() ) );
+              }
+            }
+            catch ( fc::exception& er )
+            {
+              if( con_del )
+              {
+                elog( "disconnected ${er}", ("er", er.to_detail_string() ) );
+                con_del->on_connection_disconnected( self );
+              }
+              else
+              {
+                elog( "disconnected ${e}", ("e", er.to_detail_string() ) );
+              }
+              FC_RETHROW_EXCEPTION( er, warn, "disconnected ${e}", ("e", er.to_detail_string() ) );
+            }
+            catch ( ... )
+            {
+              // TODO: call con_del->????
+              FC_THROW_EXCEPTION( unhandled_exception, "disconnected: {e}", ("e", fc::except_str() ) );
+            }
+          }
+     };
+  } // namespace detail
+
+  connection::connection( const stcp_socket_ptr& c, connection_delegate* d )
+  :my( new detail::connection_impl(*this) )
+  {
+    my->sock = c;
+    my->con_del = d;
+    my->remote_ep = remote_endpoint();
+    my->read_loop_complete = fc::async( [=](){ my->read_loop(); } );
+  }
+
+  connection::connection( connection_delegate* d )
+  :my( new detail::connection_impl(*this) ) 
+  { 
+    assert( d != nullptr );
+    my->con_del = d; 
+  }
+
+  void connection::set_last_sync_time( const fc::time_point& sync_time )
+  {
+     my->_sync_time = sync_time;
+  }
+  fc::time_point connection::get_last_sync_time()const
+  {
+     return my->_sync_time;
+  }
+
+  connection::~connection()
+  {
+    try {
+        // delegate does not get called from destructor...
+        // because shared_from_this() will return nullptr 
+        // and cause us all kinds of grief
+        my->con_del = nullptr; 
+
+        close();
+        if( my->read_loop_complete.valid() )
+        {
+          my->read_loop_complete.wait();
+        }
+        if( my->exec_sync_loop_complete.valid() )
+        {
+          my->exec_sync_loop_complete.cancel();
+          my->exec_sync_loop_complete.wait();
+        }
+    } 
+    catch ( const fc::canceled_exception& e )
+    {
+      ilog( "canceled" );
+    }
+    catch ( const fc::exception& e )
+    {
+      wlog( "unhandled exception on close:\n${e}", ("e", e.to_detail_string()) );   
+    }
+    catch ( ... )
+    {
+      elog( "unhandled exception on close ${e}", ("e", fc::except_str()) );   
+    }
+  }
+  stcp_socket_ptr connection::get_socket()const
+  {
+     return my->sock;
+  }
+
+  void connection::close()
+  {
+     try {
+         if( my->sock )
+         {
+           my->sock->get_socket().close();
+           if( my->read_loop_complete.valid() )
+           {
+              wlog( "waiting for socket to close" );
+              my->read_loop_complete.wait();
+              wlog( "socket closed" );
+           }
+         }
+     } FC_RETHROW_EXCEPTIONS( warn, "exception thrown while closing socket" );
+  }
+  void connection::connect( const fc::ip::endpoint& ep )
+  {
+     try {
+       // TODO: do we have to worry about multiple calls to connect?
+       my->sock = std::make_shared<stcp_socket>();
+       my->sock->connect_to(ep); 
+       my->remote_ep = remote_endpoint();
+       ilog( "    connected to ${ep}", ("ep", ep) );
+       my->read_loop_complete = fc::async( [=](){ my->read_loop(); } );
+     } FC_RETHROW_EXCEPTIONS( warn, "error connecting to ${ep}", ("ep",ep) );
+  }
+
+  void connection::connect( const std::string& host_port )
+  {
+      int idx = host_port.find( ':' );
+      auto eps = fc::resolve( host_port.substr( 0, idx ), fc::to_int64(host_port.substr( idx+1 )));
+      ilog( "connect to ${host_port} and resolved ${endpoints}", ("host_port", host_port)("endpoints",eps) );
+      for( auto itr = eps.begin(); itr != eps.end(); ++itr )
+      {
+         try 
+         {
+            connect( *itr );
+            return;
+         } 
+         catch ( const fc::exception& e )
+         {
+            wlog( "    attempt to connect to ${ep} failed.", ("ep", *itr) );
+         }
+      }
+      FC_THROW_EXCEPTION( exception, "unable to connect to ${host_port}", ("host_port",host_port) );
+  }
+
+  void connection::send( const message& m )
+  {
+    try {
+      fc::scoped_lock<fc::mutex> lock(my->write_lock);
+      size_t len = MAIL_PACKED_MESSAGE_HEADER + m.size;
+      len = 16*((len+15)/16); //pad the message we send to a multiple of 16 bytes
+      std::vector<char> tmp(len);
+      memcpy( tmp.data(), (char*)&m, MAIL_PACKED_MESSAGE_HEADER );
+      memcpy( tmp.data() + MAIL_PACKED_MESSAGE_HEADER, m.data.data(), m.size );
+      my->sock->write( tmp.data(), tmp.size() );
+      my->sock->flush();
+    } FC_RETHROW_EXCEPTIONS( warn, "unable to send message" );
+  }
+
+
+  fc::ip::endpoint connection::remote_endpoint()const 
+  {
+     if( get_socket()->get_socket().is_open() )
+     {
+         return my->remote_ep = get_socket()->get_socket().remote_endpoint();
+     }
+     // TODO: document why we are not throwing an exception if there is no remote endpoint?
+     return my->remote_ep;
+  }
+
+  void connection::exec_sync_loop()
+  {
+      my->exec_sync_loop_complete = fc::async( [=]() 
+      {
+          while( !my->exec_sync_loop_complete.canceled() )
+          {
+             auto itr = my->_db->lower_bound( my->_sync_time );
+             while( itr.valid() && !my->exec_sync_loop_complete.canceled() )
+             {
+                send( message( itr.value() ) );
+                my->_sync_time = itr.key();
+                ++itr;
+             }
+             fc::usleep( fc::seconds(15) );
+          }
+      });
+  }
+
+  void connection::set_database( bts::db::level_map<fc::time_point,bts::bitchat::encrypted_message>* db )
+  {
+     my->_db = db;
+  }
+
+
+} // mail
---
+++ src/mail/mail_server.cpp
@@ -0,0 +1,267 @@
+#include <mail/mail_server.hpp>
+#include <mail/mail_connection.hpp>
+#include <mail/message.hpp>
+#include <mail/stcp_socket.hpp>
+#include <bts/db/level_map.hpp>
+#include <bts/bitchat/bitchat_private_message.hpp>
+#include <bts/bitchat/bitchat_messages.hpp>
+#include <fc/time.hpp>
+#include <fc/network/tcp_socket.hpp>
+#include <fc/reflect/variant.hpp>
+#include <fc/thread/thread.hpp>
+#include <fc/thread/future.hpp>
+#include <fc/io/raw.hpp>
+#include <fc/log/logger.hpp>
+
+
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+
+namespace mail {
+
+  namespace detail
+  {
+     class server_impl : public connection_delegate
+     {
+        public:
+          server_impl()
+          :ser_del(nullptr)
+          {}
+
+          ~server_impl()
+          {
+             close();
+          }
+          void close()
+          {
+              ilog( "closing connections..." );
+              try 
+              {
+                  for( auto i = pending_connections.begin(); i != pending_connections.end(); ++i )
+                  {
+                    (*i)->close();
+                  }
+                  tcp_serv.close();
+                  if( accept_loop_complete.valid() )
+                  {
+                      accept_loop_complete.cancel();
+                      accept_loop_complete.wait();
+                  }
+              } 
+              catch ( const fc::canceled_exception& e )
+              {
+                  ilog( "expected exception on closing tcp server\n" );  
+              }
+              catch ( const fc::exception& e )
+              {
+                  wlog( "unhandled exception in destructor ${e}", ("e", e.to_detail_string() ));
+              } 
+              catch ( ... )
+              {
+                  elog( "unexpected exception" );
+              }
+          }
+          server_delegate*                                            ser_del;
+          fc::ip::address                                             _external_ip;
+
+          bts::db::level_map<fc::time_point,bts::bitchat::encrypted_message>    _message_db;
+
+          std::unordered_map<fc::ip::endpoint,connection_ptr>         connections;
+
+          std::set<connection_ptr>                                    pending_connections;
+          server::config                                              cfg;
+          fc::tcp_server                                              tcp_serv;
+                                                                     
+          fc::future<void>                                            accept_loop_complete;
+                                                                     
+          /**
+           *  This is called every time a message is received from c, there are only two
+           *  messages supported:  seek to time and broadcast.  When a message is 
+           *  received it goes into the database which all of the connections are 
+           *  reading from and sending to their clients.
+           *
+           *  The difficulty required adjusts every 5 minutes with the goal of maintaining
+           *  an average data rate of 1.5 kb/sec from all connections.
+           */
+          virtual void on_connection_message( connection& c, const message& m )
+          {
+               // TODO: perhaps do this ASYNC?
+               // itr->second->handle_message( c.shared_from_this(), m );
+               if( m.type == bts::bitchat::encrypted_message::type )
+               {
+                   auto pm = m.as<bts::bitchat::encrypted_message>();
+                   if( pm.validate_proof() )
+                   {
+                      _message_db.store( fc::time_point::now(), pm );
+                   }
+               }
+               else if( m.type == bts::bitchat::client_info_message::type )
+               {
+                   auto ci = m.as<bts::bitchat::client_info_message>();
+                   if( c.get_last_sync_time() == fc::time_point() )
+                   {
+                      c.exec_sync_loop();
+                   }
+                   c.set_last_sync_time( ci.sync_time );
+               }
+               else
+               {
+                   c.close();
+               }
+          }
+
+
+          virtual void on_connection_disconnected( connection& c )
+          {
+            try {
+              ilog( "cleaning up connection after disconnect ${e}", ("e", c.remote_endpoint()) );
+              auto cptr = c.shared_from_this();
+              FC_ASSERT( cptr );
+              if( ser_del ) ser_del->on_disconnected( cptr );
+              auto itr = connections.find(c.remote_endpoint());
+              connections.erase( itr ); //c.remote_endpoint() );
+            } FC_RETHROW_EXCEPTIONS( warn, "error thrown handling disconnect" );
+          }
+
+          /**
+           *  This method is called via async from accept_loop and
+           *  should not throw any exceptions because they are not
+           *  being caught anywhere.
+           *
+           *  
+           */
+          void accept_connection( const stcp_socket_ptr& s )
+          {
+             try 
+             {
+                // init DH handshake, TODO: this could yield.. what happens if we exit here before
+                // adding s to connections list.
+                s->accept();
+                ilog( "accepted connection from ${ep}", 
+                      ("ep", std::string(s->get_socket().remote_endpoint()) ) );
+                
+                auto con = std::make_shared<connection>(s,this);
+                connections[con->remote_endpoint()] = con;
+                if( ser_del ) ser_del->on_connected( con );
+             } 
+             catch ( const fc::canceled_exception& e )
+             {
+                ilog( "canceled accept operation" );
+             }
+             catch ( const fc::exception& e )
+             {
+                wlog( "error accepting connection: ${e}", ("e", e.to_detail_string() ) );
+             }
+             catch( ... )
+             {
+                elog( "unexpected exception" );
+             }
+          }
+
+          /**
+           *  This method is called async 
+           */
+          void accept_loop() throw()
+          {
+             try
+             {
+                while( !accept_loop_complete.canceled() )
+                {
+                   stcp_socket_ptr sock = std::make_shared<stcp_socket>();
+                   tcp_serv.accept( sock->get_socket() );
+
+                   // do the acceptance process async
+                   fc::async( [=](){ accept_connection( sock ); } );
+
+                   // limit the rate at which we accept connections to prevent
+                   // DOS attacks.
+                   fc::usleep( fc::microseconds( 1000*10 ) );
+                }
+             } 
+             catch ( fc::eof_exception& e )
+             {
+                ilog( "accept loop eof" );
+             }
+             catch ( fc::canceled_exception& e )
+             {
+                ilog( "accept loop canceled" );
+             }
+             catch ( fc::exception& e )
+             {
+                elog( "tcp server socket threw exception\n ${e}", 
+                                     ("e", e.to_detail_string() ) );
+                // TODO: notify the server delegate of the error.
+             }
+             catch( ... )
+             {
+                elog( "unexpected exception" );
+             }
+          }
+     };
+  }
+
+
+
+
+  server::server()
+  :my( new detail::server_impl() ){}
+
+  server::~server()
+  { }
+
+
+  void server::set_delegate( server_delegate* sd )
+  {
+     my->ser_del = sd;
+  }
+
+  void server::configure( const server::config& c )
+  {
+    try {
+      my->cfg = c;
+
+      ilog( "listening for stcp connections on port ${p}", ("p",c.port) );
+      my->tcp_serv.listen( c.port );
+      my->accept_loop_complete = fc::async( [=](){ my->accept_loop(); } ); 
+      my->_message_db.open( "message_db" );
+
+    } FC_RETHROW_EXCEPTIONS( warn, "error configuring server", ("config", c) );
+  }
+
+  std::vector<connection_ptr> server::get_connections()const
+  { 
+      std::vector<connection_ptr>  cons; 
+      cons.reserve( my->connections.size() );
+      for( auto itr = my->connections.begin(); itr != my->connections.end(); ++itr )
+      {
+        cons.push_back(itr->second);
+      }
+      return cons;
+  }
+
+  /*
+  void server::broadcast( const message& m )
+  {
+      for( auto itr = my->connections.begin(); itr != my->connections.end(); ++itr )
+      {
+        try {
+           itr->second->send(m);
+        } 
+        catch ( const fc::exception& e ) 
+        {
+           // TODO: propagate this exception back via the delegate or some other means... don't just fail
+           wlog( "exception thrown while broadcasting ${e}", ("e", e.to_detail_string() ) );
+        }
+      }
+  }
+  */
+
+  void server::close()
+  {
+    try {
+      my->close();
+    } FC_RETHROW_EXCEPTIONS( warn, "error closing server socket" );
+  }
+
+} // namespace mail
